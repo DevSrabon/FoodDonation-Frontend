d418497e2226c45084601ba33abe2664e9c84fa6
MahbubMorshed <mahbub.bogra30@gmail.com>
2023-06-21 03:42:21 +0600

    Login complete

--- src/screens/InitialPage.js
+++ src/screens/InitialPage.js
@@ -1,49 +1,60 @@
-import { View, Text, FlatList, Pressable } from "react-native";
+import { View, Text, Pressable, StyleSheet, Image } from "react-native";
 import React from "react";
+import icons from "../../assets/icons";
 import { useNavigation } from "@react-navigation/native";
-
-const data = [
-  {
-    id: "1",
-    header: "Donate Food",
-    details:
-      "Food Donation is a Noble act so don’t let a person suffer from hunger",
-  },
-  {
-    id: "2",
-    header: "Publish Post",
-    details:
-      "Through a post a donor can publish about the availability of the food",
-  },
-  {
-    id: "3",
-    header: "Get Food Faster",
-    details: "Giving the access to the people who can deliver the food",
-  },
-];
 
 const InitialPage = () => {
   const navigation = useNavigation();
   return (
-    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      <Text> Initial </Text>
-      <Pressable onPress={() => navigation.navigate("intro")}>
-        <Text>Next Page</Text>
-      </Pressable>
-      {/* <FlatList
-        data={data}
-        renderItem={({ item }) => (
-          <View>
-            <Text>{data.header}</Text>
-            <Text>{data.details}</Text>
-          </View>
-        )}
-        horizontal
-        showsHorizontalScrollIndicator={false}
-        pagingEnabled
-      /> */}
+    <View style={styles.container}>
+      <Text style={styles.header}> Food Donation. </Text>
+
+      <View style={{ paddingRight: 45, paddingLeft: 30 }}>
+        <Text style={styles.detail}>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Risus, est ac
+          iaculis euismod
+        </Text>
+
+        <Pressable onPress={() => navigation.navigate("intro")}>
+          <Image source={icons.InitialBtn} style={styles.icon} />
+        </Pressable>
+      </View>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "#B4AAF2",
+    // marginTop: StatusBar.currentHeight,
+  },
+  header: {
+    position: "absolute",
+    fontFamily: "Bold",
+    fontSize: 40,
+    lineHeight: 46,
+    textAlign: "center",
+    justifyContent: "center",
+    // paddingHorizontal: 20,
+    marginHorizontal: 70,
+  },
+  detail: {
+    fontFamily: "Reguler",
+    paddingLeft: 30,
+    fontSize: 15,
+    lineHeight: 22,
+    textAlign: "center",
+    // paddingTop: 100,
+    marginTop: 400,
+  },
+  icon: {
+    marginRight: 220,
+    marginTop: 120,
+  },
+});
 
 export default InitialPage;
--- src/screens/Login.js
+++ src/screens/Login.js
@@ -1,15 +1,159 @@
-import { View, Text, Pressable } from "react-native";
-import React from "react";
+import { View, Text, Pressable, Image, StyleSheet } from "react-native";
+import React, { useState } from "react";
+import CustomInput from "../components/CustomInput";
+import CustomButton from "../components/CustomButton";
+import icons from "../../assets/icons";
+import { useNavigation } from "@react-navigation/native";
+import Checkbox from "expo-checkbox";
 
 const Login = () => {
+  const navigation = useNavigation();
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [isChecked, setChecked] = useState(false);
+
+  const onSignInPressed = () => {
+    console.warn("signin");
+  };
+
+  const onForgotPasswordPressed = () => {
+    console.warn("Forgot Password");
+  };
+
+  const onSignInFacebook = () => {
+    console.warn("Facebook");
+  };
+
+  const onSignInGoogle = () => {
+    console.warn("Google");
+  };
+
+  const onSignInLinkedin = () => {
+    console.warn("linkedin");
+  };
+
+  const onSignup = () => {
+    navigation.navigate("signup");
+  };
+
   return (
-    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      <Text>Login</Text>
-      <Pressable onPress={() => navigation.navigate("signup")}>
-        <Text>signup</Text>
-      </Pressable>
+    <View style={styles.container}>
+      <Text
+        style={{ fontFamily: "SemiBold", fontSize: 28, bottom: 20, right: 145 }}
+      >
+        Login
+      </Text>
+      <Text
+        style={{ fontFamily: "SemiBold", fontSize: 14, right: 160, top: 6 }}
+      >
+        E-mail
+      </Text>
+
+      <CustomInput
+        placeholder="Username"
+        value={username}
+        setValue={setUsername}
+      />
+
+      <Text
+        style={{ fontFamily: "SemiBold", fontSize: 14, right: 150, top: 6 }}
+      >
+        Password
+      </Text>
+      <CustomInput
+        placeholder="Password"
+        value={password}
+        setValue={setPassword}
+        secureTextEntry={true}
+      />
+      <View
+        style={{
+          flexDirection: "row",
+          alignItems: "center",
+          gap: 130,
+          marginBottom: 20,
+        }}
+      >
+        <View style={{ flexDirection: "row" }}>
+          <Checkbox
+            style={styles.checkbox}
+            value={isChecked}
+            onValueChange={setChecked}
+            color={isChecked ? "#B4AAF2" : undefined}
+          />
+          <Text
+            style={{ fontFamily: "Medium", fontSize: 12, color: "#747980" }}
+          >
+            Remember me
+          </Text>
+        </View>
+
+        <CustomButton
+          text="Forgot Password"
+          onPress={onForgotPasswordPressed}
+          type="tertiary"
+        />
+      </View>
+
+      <CustomButton text="Login" onPress={onSignInPressed} type="primary" />
+
+      <Text
+        style={{
+          fontFamily: "SemiBold",
+          fontSize: 12,
+          marginBottom: 10,
+          marginTop: 20,
+        }}
+      >
+        or continue with
+      </Text>
+      <View style={styles.subContainer}>
+        <Pressable style={styles.box}>
+          <Image source={icons.fb} />
+        </Pressable>
+        <Pressable style={styles.box}>
+          <Image source={icons.google} />
+        </Pressable>
+        <Pressable style={styles.box}>
+          <Image source={icons.linked} />
+        </Pressable>
+      </View>
+      <View
+        style={{
+          flexDirection: "row",
+          alignItems: "center",
+          top: 80,
+        }}
+      >
+        <Text style={{ fontFamily: "SemiBold", fontSize: 12 }}>
+          Don,t have an account?
+        </Text>
+        <CustomButton text="Signup" onPress={onSignup} type="tertiary" />
+      </View>
     </View>
   );
 };
 
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "white",
+  },
+  subContainer: {
+    flexDirection: "row",
+
+    gap: 20,
+  },
+  box: {
+    width: 72,
+    heigh: 68,
+    padding: 16,
+    borderRadius: 6,
+    borderColor: "#EBE9F1",
+    borderWidth: 1,
+  },
+  checkbox: { marginRight: 3 },
+});
 export default Login;
