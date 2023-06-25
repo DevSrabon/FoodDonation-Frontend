bc1e5367a37b7fd2463e1ea7f06a6e4519c7a6ae
Md-Golam-Mostafa <96967187+Md-Golam-Mostafa@users.noreply.github.com>
2023-06-26 05:40:21 +0600

    map functionality updated

--- src/screens/UserCountWithinRadius.js
+++ src/screens/UserCountWithinRadius.js
@@ -1,67 +1,39 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
+import { View, Text, Button, Alert } from 'react-native';
 import axios from 'axios';
-import { Text } from 'react-native';
 
 const UserCountWithinRadius = () => {
   const [userCount, setUserCount] = useState(0);
 
-  useEffect(() => {
-    const fetchUserCountWithinRadius = async () => {
-      try {
-        const userLocation = { latitude: 37.7749, longitude: -122.4194 }; //  specific location
+  const handleGetUserCount = async () => {
+    console.log("handle user count=====", userCount);
+    try {
+      const response = await axios.get('http://localhost:4000/api/users/count', {
 
-        const response = await axios.get('http://localhost:3000/users'); //  API endpoint
+        params: {
+          latitude: 23.0786,
+          longitude: 91.3336,
+          radius: 5000, // 5 km radius
+        },
+      });
 
-        const users = response.data; // API response contains an array of user objects
-        const usersWithinRadius = getUsersWithinRadius(users, userLocation, 5); // Calculate users within a 5km radius
+      console.log("response data====", response);
 
-        setUserCount(usersWithinRadius.length);
-      } catch (error) {
-        console.error('Error fetching user count:', error);
-      }
-    };
-
-    fetchUserCountWithinRadius();
-  }, []);
-
-  const getUsersWithinRadius = (users, location, radius) => {
-    const usersWithinRadius = [];
-
-    users.forEach((user) => {
-      const userLocation = { latitude: user.latitude, longitude: user.longitude }; // Assuming each user object has latitude and longitude properties
-
-      const distance = calculateDistance(location, userLocation);
-
-      if (distance <= radius) {
-        usersWithinRadius.push(user);
-      }
-    });
-
-    return usersWithinRadius;
-  };
-
-  const calculateDistance = (location1, location2) => {
-    const earthRadius = 6371; // Radius of the Earth in kilometers
-
-    const latDiff = toRad(location2.latitude - location1.latitude);
-    const lonDiff = toRad(location2.longitude - location1.longitude);
-
-    const a =
-      Math.sin(latDiff / 2) * Math.sin(latDiff / 2) +
-      Math.cos(toRad(location1.latitude)) * Math.cos(toRad(location2.latitude)) * Math.sin(lonDiff / 2) * Math.sin(lonDiff / 2);
-    const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
-    const distance = earthRadius * c;
-
-    return distance;
-  };
-
-  const toRad = (value) => {
-    return (value * Math.PI) / 180;
+      const { count } = response.data;
+      setUserCount(count);
+    } catch (error) {
+      console.error('Error getting user count:', error);
+      Alert.alert('Error', 'Failed to get user count. Please try again.');
+    }
   };
 
   return (
-    <Text>Number of users within a 5km radius: {userCount}</Text>
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Number of Users: {userCount}</Text>
+      <Button title="Get User Count" onPress={handleGetUserCount} />
+    </View>
   );
 };
 
 export default UserCountWithinRadius;
+
