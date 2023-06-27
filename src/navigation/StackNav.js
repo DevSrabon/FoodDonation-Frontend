e80a3a0138c109da8afabc63968a86c9d40ec6c5
devSrabon <et.srabon@gmail.com>
2023-06-27 18:39:12 +0600

    route change

--- src/navigation/StackNav.js
+++ src/navigation/StackNav.js
@@ -27,7 +27,7 @@ const StackNav = () => {
   return (
     <NavigationContainer>
       <Stack.Navigator
-        initialRouteName="AddRestaurant"
+        initialRouteName="initial"
         screenOptions={{ headerShown: false }}
       >
         <Stack.Screen name="initial" component={InitialPage} />
