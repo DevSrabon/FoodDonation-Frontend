aea6e4408e9b8e4833f6f4a6893b46f028d3f7d4
Md-Golam-Mostafa <96967187+Md-Golam-Mostafa@users.noreply.github.com>
2023-07-03 06:53:16 +0600

    time limit added

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Image, ScrollView, StyleSheet, Text, View } from "react-native";
 import icons from "../../assets/icons";
 import Loading from "../components/Loading";
@@ -6,53 +6,16 @@ import SearchHeader from "../components/SearchHeader";
 import Container from "../components/container";
 import { userContext } from "../context/Provider";
 import useFetchData from "../hook/useFetchData";
+import TimeLimitComponent from "./TimeLimitComponent";
 
 const Home = () => {
-  const [search, setSearch] = useState(0);
   const { allData } = userContext();
   console.log("🚀 ~ file: Home.js:13 ~ Home ~ allData:", allData);
 
-  const [timeRemaining, setTimeRemaining] = useState(0);
   const { loading, error, data } = useFetchData(
     `posts/getPost?role=${allData?.userData?.role || allData?.guestData}`
   );
 
-  // useEffect(() => {
-  //   const timeLimit = 25 * 60 * 1000; // 25 minutes in milliseconds
-  //   const startTime = new Date(allData.userData.createdAt).getTime();
-  //   const endTime = startTime + timeLimit;
-
-
-  //   const updateRemainingTime = () => {
-  //     const currentTime = new Date().getTime();
-  //     const remainingTime = endTime - currentTime;
-
-  //     if (remainingTime > 0) {
-  //       setTimeRemaining(remainingTime);
-  //     } else {
-  //       // Time limit reached, perform desired actions here
-  //       // console.log('Time limit reached!');
-  //       setTimeRemaining(0);
-  //     }
-  //   };
-
-  //   const intervalId = setInterval(updateRemainingTime, 1000);
-
-  //   return () => {
-  //     clearInterval(intervalId);
-  //   };
-  // }, [allData.userData.createdAt]);
-
-  // const formatTime = (timeInMilliseconds) => {
-  //   const minutes = Math.floor(timeInMilliseconds / (1000 * 60));
-  //   const seconds = Math.floor((timeInMilliseconds % (1000 * 60)) / 1000);
-  //   return `${minutes.toString().padStart(2, "0")}:${seconds
-  //     .toString()
-  //     .padStart(2, "0")}`;
-  // };
-
-
-  // console.log("all user data ===", allData.userData.createdAt);
 
   if (loading) return <Loading />;
   if (error) return alert(error.message);
@@ -62,7 +25,6 @@ const Home = () => {
       <ScrollView style={{ flex: 1, bottom: 250 }}>
         {data?.map((item) => (
           <View key={item._id} style={styles.cardContainer}>
-            {/* <Text>Time Remaining: {formatTime(timeRemaining)}</Text> */}
             <Image
               source={{ uri: item?.imageUrls?.[0] } || icons.fixedHeight} // Replace with the path to your image
               style={styles.cardImage}
@@ -72,7 +34,7 @@ const Home = () => {
             <View style={styles.profileContainer}>
               <View style={styles.imageContainerProfile}>
                 <Image
-                  source={icons.profile} // Replace with the path to your image
+                  source={icons.profile} 
                   style={styles.profileImage}
                   resizeMode="cover"
                 />
@@ -86,9 +48,8 @@ const Home = () => {
             </View>
             <View style={styles.contentCard}>
               <View style={styles.cardItemsContainer}>
-
                 <Text style={styles.textItem1}>
-                  {/* {timeRemaining ? "Available" : "Expired"} */}
+                   <TimeLimitComponent key={item._id} previousTime={item.updatedAt}></TimeLimitComponent>
                 </Text>
                 <Text style={styles.textItem2}>Dinner</Text>
               </View>
