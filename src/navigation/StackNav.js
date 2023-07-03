db422f07e6252691b88ab0b3e5ed8e968a3ebec8
Anant7000 <127724636+Anant7000@users.noreply.github.com>
2023-07-03 12:10:56 +0530

    ashu

--- src/navigation/StackNav.js
+++ src/navigation/StackNav.js
@@ -31,7 +31,7 @@ const StackNav = () => {
   return (
     <NavigationContainer>
       <Stack.Navigator
-        initialRouteName="initial"
+        initialRouteName="chat"
         screenOptions={{ headerShown: false }}
       >
         <Stack.Screen name="initial" component={InitialPage} />
