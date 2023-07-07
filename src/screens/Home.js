b9a8d64a192c1cf040f2f58e0ef5c10ca1992b09
Naman Saini <namansaini033@gmail.com>
2023-07-07 20:48:48 +0530

    integrated login and signUp screen

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,13 +1,17 @@
 import { useNavigation } from "@react-navigation/native";
 import React from "react";
+import * as Sharing from 'expo-sharing';
 import {
   Image,
   Pressable,
+  Button,
+  Share,
   ScrollView,
   StyleSheet,
   Text,
   View,
 } from "react-native";
+import * as FileSystem from 'expo-file-system';
 import icons from "../../assets/icons";
 import Loading from "../components/Loading";
 import SearchHeader from "../components/SearchHeader";
@@ -15,6 +19,7 @@ import Container from "../components/container";
 import { userContext } from "../context/Provider";
 import useFetchData from "../hook/useFetchData";
 import TimeLimitComponent from "./TimeLimitComponent";
+import ShareScreen from "../components/Share";
 
 const Home = () => {
   const { allData } = userContext();
@@ -25,6 +30,29 @@ const Home = () => {
   const navigation = useNavigation();
   if (loading) return <Loading />;
   if (error) return alert(error.message);
+  const onShare = async (post) => {
+    console.log("inside",post?.imageUrls?.[0]);
+    try {
+      const { uri: localUri } = await FileSystem.downloadAsync(post?.imageUrls?.[0], FileSystem.documentDirectory + 'image.jpg');
+      const result =  await Sharing.shareAsync(localUri, { dialogTitle: post.title });
+    
+        if (result.action === Share.sharedAction) {
+            if (result.activityType) {
+                console.log('shared with activity type of : ', result.activityType)
+
+            } else {
+                console.log('shared')
+            }
+
+        }
+        else if(result.action===Share.dismissedAction){
+            console.log('dismissed')
+        }
+    }catch(error){
+        console.log(error.message)
+
+    }
+}
   return (
     <Container>
       <SearchHeader />
@@ -70,6 +98,8 @@ const Home = () => {
                 <Text style={styles.textItem2}>Dinner</Text>
               </View>
             </View>
+            <Button title='share' onPress={()=>onShare(item)}/>
+
           </View>
         ))}
       </ScrollView>
