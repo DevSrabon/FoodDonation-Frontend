8df1eb648743fe1918d6ade86d7375b91ee851e7
Aryan305 <aryan.solanki@bombaysoftwares.com>
2023-07-10 22:58:29 +0530

    Notification visible now

--- src/screens/BackgroundFetchScreen.js
+++ src/screens/BackgroundFetchScreen.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState,useRef,useEffect } from "react";
 import * as BackgroundFetch from "expo-background-fetch";
 import * as TaskManager from "expo-task-manager";
 import { StyleSheet, View, Button, Platform, Text } from "react-native";
@@ -7,96 +7,49 @@ import * as Notifications from "expo-notifications";
 import { userContext } from "../context/Provider";
 import axios from "axios";
 import { useNavigation } from "@react-navigation/native";
-const BACKGROUND_FETCH_TASK = "background-fetch";
-
-TaskManager.defineTask(BACKGROUND_FETCH_TASK, async () => {
-  const now = Date.now();
-  console.log(
-    `Got background fetch call at date: ${new Date(now).toISOString()}`
-  );
-
-  (async () => {
-    try {
-      await schedulePushNotification();
-    } catch (error) {
-      console.error("Error ====", error);
-    }
-  })();
-
-  await schedulePushNotification("Notification Title", "Notification Body", {
-    data: "The data we want",
-  });
-
-  return BackgroundFetch.BackgroundFetchResult.NewData;
+Notifications.setNotificationHandler({
+  handleNotification: async () => ({
+    shouldShowAlert: true,
+    shouldPlaySound: false,
+    shouldSetBadge: false,
+  }),
 });
 
-async function schedulePushNotification(title, body, data) {
-  const isNotificationScheduled =
-    await Notifications.getAllScheduledNotificationsAsync();
-
-  if (isNotificationScheduled.length === 0) {
-    await Notifications.scheduleNotificationAsync({
-      content: {
-        title: title,
-        body: body,
-        data: data,
-        // title: "You've got notification! 📬",
-        // body: 'Here is the notification body',
-        // data: { data: 'goes here' },
-      },
-      trigger: { seconds: 1 }, // Remove the trigger to schedule the notification immediately
-    });
-  }
-  (async () => {
-    try {
-        await schedulePushNotification();
-    } catch (error) {
-        console.error('Error ====', error);
-    }
-})();
-  console.log("Notification scheduled successfully!");
+const showNotification = (title, body, data)=>{
+  Notifications.scheduleNotificationAsync({
+  content: {
+    title: title,
+    body: body,
+    data:data
+  },
+  trigger: null,
+});
 }
 
-async function registerBackgroundFetchAsync() {
-  return BackgroundFetch.registerTaskAsync(BACKGROUND_FETCH_TASK, {
-    minimumInterval: 1 * 15, // 15 second
-    stopOnTerminate: false, // android only,
-    startOnBoot: true, // android only
-  });
-}
 
-async function unregisterBackgroundFetchAsync() {
-  return BackgroundFetch.unregisterTaskAsync(BACKGROUND_FETCH_TASK);
-}
-
-const BackgroundFetchScreen = () => {
-    const {user,setAllData} = userContext();
-  const [expoPushToken, setExpoPushToken] = React.useState("");
-  const notificationListener = React.useRef();
-  const responseListener = React.useRef();
-  const [notification, setNotification] = React.useState(false);
+export default function BackgroundFetchScreen() {
+  const {user,setAllData} = userContext();
+  const [expoPushToken, setExpoPushToken] = useState('');
+  const [notification, setNotification] = useState(false);
+  const notificationListener = useRef();
+  const responseListener = useRef();
   const [state, setState] = useState(false);
   const navigation = useNavigation();
-  React.useEffect(() => {
-    registerForPushNotificationsAsync().then((token) =>
-      setExpoPushToken(token)
-    );
-    notificationListener.current =
-      Notifications.addNotificationReceivedListener((notification) => {
-        setNotification(notification);
-      });
 
-    responseListener.current =
-      Notifications.addNotificationResponseReceivedListener((response) => {
-        console.log(response);
-      });
+  useEffect(() => {
+    registerForPushNotificationsAsync().then(token => setExpoPushToken(token));
+
+    notificationListener.current = Notifications.addNotificationReceivedListener(notification => {
+      setNotification(notification);
+    });
+
+    responseListener.current = Notifications.addNotificationResponseReceivedListener(response => {
+      console.log(response);
+    });
 
     return () => {
-      Notifications.removeNotificationSubscription(
-        notificationListener.current
-      );
+      Notifications.removeNotificationSubscription(notificationListener.current);
       Notifications.removeNotificationSubscription(responseListener.current);
-      setState(true);
     };
   }, []);
   const checkForOrderStatus = async () => {
@@ -117,13 +70,13 @@ const BackgroundFetchScreen = () => {
             "Your order has been accepted by a volunteer",
             { data: "goes here" }
           );
+          showNotification("Order Accepted","Your order has been accepted by a volunteer","goes here" )
         }
       } catch (error) {
         console.log("notification not triggred", error);
       }
     }
   };
-
   React.useEffect(() => {
     const interval = setInterval(() => {
       checkForOrderStatus();
@@ -134,109 +87,57 @@ const BackgroundFetchScreen = () => {
     return () => clearInterval(interval);
   }, [state]);
 
-  async function registerForPushNotificationsAsync() {
-    let token;
-
-    if (Platform.OS === "android") {
-      await Notifications.setNotificationChannelAsync("default", {
-        name: "default",
-        importance: Notifications.AndroidImportance.MAX,
-        vibrationPattern: [0, 250, 250, 250],
-        lightColor: "#FF231F7C",
-      });
-    }
-
-    if (Device.isDevice) {
-      const { status: existingStatus } =
-        await Notifications.getPermissionsAsync();
-      let finalStatus = existingStatus;
-      if (existingStatus !== "granted") {
-        const { status } = await Notifications.requestPermissionsAsync();
-        finalStatus = status;
-      }
-
-      if (finalStatus !== "granted") {
-        alert("Failed to get push token for push notification!");
-        return;
-      }
-      token = (await Notifications.getExpoPushTokenAsync()).data;
-      console.log("push token =====", token);
-    } else {
-      console.log("Must use physical device for Push Notifications");
-    }
-    return token;
-  }
-
-  const [isRegistered, setIsRegistered] = React.useState(false);
-  const [status, setStatus] = React.useState(null);
   const goToForegroundScreen = () => {
     navigation.navigate("user");
   };
 
   React.useEffect(() => {
-    checkStatusAsync();
     console.log("Background screen running");
-    setTimeout(goToForegroundScreen, 1000);
+    setTimeout(goToForegroundScreen, 500);
   }, []);
+  return (null
+  );
+}
 
-  const checkStatusAsync = async () => {
-    const status = await BackgroundFetch.getStatusAsync();
-    const isRegistered = await TaskManager.isTaskRegisteredAsync(
-      BACKGROUND_FETCH_TASK
-    );
-    setStatus(status);
-    setIsRegistered(isRegistered);
-  };
+async function schedulePushNotification() {
+  await Notifications.scheduleNotificationAsync({
+    content: {
+      title: "You've got mail! 📬",
+      body: 'Here is the notification body',
+      data: { data: 'goes here' },
+    },
+    trigger: { seconds: 2 },
+  });
+}
 
-  const toggleFetchTask = async () => {
-    if (isRegistered) {
-      await unregisterBackgroundFetchAsync();
-    } else {
-      await registerBackgroundFetchAsync();
+async function registerForPushNotificationsAsync() {
+  let token;
+
+  if (Platform.OS === 'android') {
+    await Notifications.setNotificationChannelAsync('default', {
+      name: 'default',
+      importance: Notifications.AndroidImportance.MAX,
+      vibrationPattern: [0, 250, 250, 250],
+      lightColor: '#FF231F7C',
+    });
+  }
+
+  if (Device.isDevice) {
+    const { status: existingStatus } = await Notifications.getPermissionsAsync();
+    let finalStatus = existingStatus;
+    if (existingStatus !== 'granted') {
+      const { status } = await Notifications.requestPermissionsAsync();
+      finalStatus = status;
     }
-    checkStatusAsync();
-  };
+    if (finalStatus !== 'granted') {
+      alert('Failed to get push token for push notification!');
+      return;
+    }
+    token = (await Notifications.getExpoPushTokenAsync()).data;
+    console.log(token);
+  } else {
+    alert('Must use physical device for Push Notifications');
+  }
 
-  return (
-//     <View style={styles.screen}>
-//       <View style={styles.textContainer}>
-//         <Text>
-//           Background fetch status:{" "}
-//           <Text style={styles.boldText}>
-//             {status && BackgroundFetch.BackgroundFetchStatus[status]}
-//           </Text>
-//         </Text>
-//         <Text>
-//           Background fetch task name:{" "}
-//           <Text style={styles.boldText}>
-//             {isRegistered ? BACKGROUND_FETCH_TASK : "Not registered yet!"}
-//           </Text>
-//         </Text>
-//       </View>
-//       <View style={styles.textContainer}></View>
-//       <Button
-//         title={
-//           isRegistered
-//             ? "Unregister BackgroundFetch task"
-//             : "Register BackgroundFetch task"
-//         }
-//         onPress={toggleFetchTask}
-//       />
-//     </View>
-  null);
-};
-
-const styles = StyleSheet.create({
-  screen: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-  },
-  textContainer: {
-    margin: 10,
-  },
-  boldText: {
-    fontWeight: "bold",
-  },
-});
-export default BackgroundFetchScreen;
+  return token;
+}
