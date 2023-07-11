b67b4c3bebe0b57ff55c5c301c6b2ad46f143932
Rishav Mukherjee <rishavmukherjee11@gmail.com>
2023-07-12 01:52:46 +0530

    working chat pics

--- src/screens/Chat.js
+++ src/screens/Chat.js
@@ -87,7 +87,7 @@ const Users = () => {
   const handleUserPress = (userchatId, user, emaill) => {
     navigation.navigate("SecuredChat", { userchatId, user, emaill });
   };
-
+//console.log(users)
   return (
     <View style={{ marginTop: 30, marginBottom: 50 }}>
       <ScrollView>
@@ -110,9 +110,16 @@ const Users = () => {
               }}
             >
               <Image
-  source={user.profileImage ? { uri: user.profileImage } : require("../../assets/icons/profile.png")}
+           source={
+            typeof user.profileImage === 'string'
+      ? { uri: user.profileImage }
+      : user.profileImage
+  
+  }
   style={{ width: 60, height: 60, borderRadius: 30 }}
 />
+
+
 
               <View>
                 <Text
